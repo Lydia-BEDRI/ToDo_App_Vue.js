3fa457e6d1624fabf47d566acdc7d3e305c0b6ab
Lydia-BEDRI <bedrilydia118@gmail.com>
2025-04-19 22:18:21 +0200

    init project + Task component

---
+++ src/components/Task.vue
@@ -0,0 +1,31 @@
+<script setup>
+const props = defineProps({
+  task: {
+    type: Object,
+    required: true
+  }
+});
+
+</script>
+
+<template>
+      <div class="task">
+        <h3>
+          {{ task.name }}
+        </h3>
+        <p>
+          {{ task.description }}
+        </p>
+        <div class="task-check">
+          <input type="checkbox" :checked="task.completed" />
+          <label>
+            Done
+          </label>
+        </div>
+      </div>
+
+</template>
+
+<style scoped>
+  
+</style>
